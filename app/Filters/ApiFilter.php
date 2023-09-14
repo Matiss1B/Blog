b28cbe4d78cfeebb04a85256aef3e22cef86c335
Matiss1B <103557980+Matiss1B@users.noreply.github.com>
2023-09-14 22:10:05 +0300

    My own api filtration

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -11,15 +11,19 @@ class ApiFilter{
     public function transform (Request $request) {
         $queries = [];
         $finalQueries = [];
+        //Loop through all keys what are allowed for filtration
         foreach ($this->allowed as $param) {
-            $query = $request->input($param);
+            $query = $request->query($param);
+            //If key what is allowed to filtration is in url, then save it
            if ($query == null) {
                 continue;
             }else{
                 $queries[$param] = $query;
             }
         }
+        //Loop through all alowed url queries, transform and push into $finalQueries array
         foreach ($queries as $queryKey => $query){
+            //if query is an array, it means, that there is special symbol, like ">" or "<", Example(id[>]=1)
             if(is_array($query)){
                 foreach ($query as $key => $value) {
                     $symbol = array_search($key, $this->allowedSymbols);
@@ -28,6 +32,7 @@ class ApiFilter{
                     }
                     break;
                 }
+                //If its not an array, that means, we need return "=", Example(id=1)
             }else{
                 $finalQueries[] = [$queryKey, "=", $query];
             }
